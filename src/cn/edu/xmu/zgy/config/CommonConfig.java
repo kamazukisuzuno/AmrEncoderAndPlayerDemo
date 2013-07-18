c4a993a2a70ece33281d1f52996b71074e78b5c8
suzuno <kamazukisuzuno@outlook.com>
2013-07-18 17:46:33 +0800

    Try Commit By Studio

--- src/cn/edu/xmu/zgy/config/CommonConfig.java
+++ src/cn/edu/xmu/zgy/config/CommonConfig.java
@@ -4,7 +4,7 @@ package cn.edu.xmu.zgy.config;
 //Email: zgyulongfei@gmail.com
 
 public class CommonConfig {
-	public static String SERVER_IP_ADDRESS = "192.168.1.145";
+	public static String SERVER_IP_ADDRESS = "192.168.16.103";
 
 	public static final int AUDIO_SERVER_UP_PORT = 20086;
 	public static final int AUDIO_SERVER_DOWN_PORT = 20087;
